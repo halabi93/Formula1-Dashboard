382431995053cf1d92adedbfd476ecce8244f71f
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2022-04-13 14:02:25 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,70 @@
+const queryUrl = "https://ergast.com/api/f1/2022/drivers.json?"
+let dropdownMenu = d3.select("#selDataset");
+
 function init() {
-    let dropdownMenu = d3.select("#selDataset");
-    console.log("obo");
-    const queryUrl = "https://ergast.com/api/f1/2022/drivers.json?"
+    
+
 
     d3.json(queryUrl).then(function (data) {
-        console.log(data);
-        console.log("bob");
+        // console.log(data);
         drivers2022 = data.MRData.DriverTable.Drivers;
+        
         console.log(drivers2022)
 
-        drivers2022.forEach((i)=>{dropdownMenu.append("option").text(i).property("value");
+        drivers2022_list = [];
+        for (let i in drivers2022) {
+          drivers2022_list.push(drivers2022[i].givenName + " " + drivers2022[i].familyName);
+        };
+
+        drivers2022_list.forEach((i)=>{dropdownMenu.append("option").text(i).property("value").code;
       })
+        demographics(drivers2022_list[0]);
+    })};
+
+function demographics(userInput) {
+  d3.json(queryUrl).then(function (data) {
+
+      // The demographics info is in data.metadata
+      // meta is the list of all metadata dictionaries
+      drivers2022 = data.MRData.DriverTable.Drivers;          
+      // meta = data.metadata;
+
+      //filter... within meta (aka data.metadata) match record.ID (record is a single dictionary in the metadata list of dictionaries) to the userInput
+      //selected is a all records (dictionaries) that match the ID of the userInput, of which there should only be one
+      // selected = meta.filter((record)=>record.id == userInput);
+      selected = drivers2022.filter((record)=>(record.givenName + " " + record.familyName) == userInput);
+      console.log(selected)
+      //firstID is the first dictionary in the list of matching records, of which there should only have been one
+      firstID = selected[0];
+      console.log(firstID)
+      let metaBox = d3.select("#sample-metadata");
+
+      //A json dictionary is a js object. Object.entries() "returns an array of a given object's own enumerable string-keyed property [key, value] pairs".
+      // select the div with id(#) "sample-metadata" (aka metaBox) and append the key: value pairs in the given text format
+      Object.entries(firstID).forEach(([key, value])=>{metaBox.append("option").text(`${key}: ${value}`);
+      })
+})};
+      
+  
+
+// This function is called when a dropdown menu item is selected
+function optionChanged(value) {
+   
+  //empty the "Demographic Info" box
+  let metaBox = d3.select("#sample-metadata");
+  metaBox.selectAll("*").remove();
+
+  demographics(value);
+  // charts(value);
+  // gauge(value);
+
+};
 
 
 
-
-      });
-      // 
-
-    // var dropdownMenu = d3.select("#selDataset");
-<<<<<<< HEAD
-      d3.json("../../Resources/samples.json").then(data => {
-=======
-    //   d3.json("C:/Users/emera/Documents/GitHub/Project3-Formula1/Resources/samples.json").then(data => {
->>>>>>> d2005c7b95b0acd57788f332ddf11d8a3a7e2993
-            // console.log(data);
-           
-          // Fill out dropdown menu
-        //   sampleID = data.names;
-        //   //for each loop... for each i in the list found in data.names (aka sampleID), append the value (ex: 940), as text, to the bottom of the "options" in the dropdown list
-        //   sampleID.forEach((i)=>{dropdownMenu.append("option").text(i).property("value");
-        //   })
-
-        // //   demographics(sampleID[0]);
         //   charts(sampleID[0]);
         // //   gauge(sampleID[0]);
-      };
+      // };
 
     //   function charts (userInput){
     //     d3.csv("../../Resources/F1_Constructor_Standings.csv").then(data => {
@@ -76,7 +105,7 @@ function init() {
       
           
       
-    //     }
+        
     // )};
 
     init();
